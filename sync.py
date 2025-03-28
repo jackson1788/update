11e853daed90cf882ce3d11d1d02c17e1479c24e
jackson1788 <mmmmjackson6@gmail.com>
2025-03-29 01:58:43 +0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -1,4 +1,11 @@
 import subprocess
+import os
 
-# 使用 subprocess 来执行 shell 命令
-subprocess.run(["git", "clone", "https://github.com/jackson1788/github-issue-sync.git"], check=True)
+# 从环境变量中获取 GitHub Token
+github_token = os.getenv('GH_TOKEN')
+
+# 创建带认证的 GitHub 仓库 URL
+repo_url = f"https://{github_token}@github.com/jackson1788/github-issue-sync.git"
+
+# 克隆 GitHub 仓库
+subprocess.run(["git", "clone", repo_url], check=True)
