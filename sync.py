f1e441d1e24d0cfb335b924bb86a4b9f46b92669
jackson1788 <mmmmjackson6@gmail.com>
2025-03-28 23:53:04 +0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -1,2 +1,4 @@
-git clone https://github.com/jackson1788/github-issue-sync.git
-cd github-issue-sync
+import subprocess
+
+# 使用 subprocess 来执行 shell 命令
+subprocess.run(["git", "clone", "https://github.com/jackson1788/github-issue-sync.git"], check=True)
