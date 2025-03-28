483bd2d4b589c9d97d77e187f5cd62c62d41522c
jackson1788 <mmmmjackson6@gmail.com>
2025-03-29 02:01:42 +0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -4,6 +4,10 @@ import os
 # 从环境变量中获取 GitHub Token
 github_token = os.getenv('GH_TOKEN')
 
+# 检查 token 是否为空，如果为空，抛出异常
+if not github_token:
+    raise ValueError("GitHub Token (GH_TOKEN) is not set or is empty.")
+
 # 创建带认证的 GitHub 仓库 URL
 repo_url = f"https://{github_token}@github.com/jackson1788/github-issue-sync.git"
 
